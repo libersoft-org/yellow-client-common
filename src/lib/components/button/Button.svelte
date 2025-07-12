ae99e3b04cb2711c58341a3545c10c94b68ef441
LiberSoft <info@libersoft.org>
2025-07-12 12:38:38 +0200

    Remove paraglide

--- src/lib/components/button/Button.svelte
+++ src/lib/components/button/Button.svelte
@@ -1,13 +1,8 @@
 <script lang="ts">
-
 	let { onClick, text } = $props();
-
 </script>
 
-bbb<button
-	class="button"
-	on:click={onClick}
->
+bbb<button class="button" on:click={onClick}>
 	{#if text}
 		<span class="label">{text}</span>
 	{/if}
